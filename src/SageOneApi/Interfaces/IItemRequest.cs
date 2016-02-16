7f65340ca10425b363fc11ec8b9119a64add0c3b
Pieter van der Westhuizen <pieter@csoft.co.za>
2016-02-16 14:59:17 +0200

    Add Items Get & Save Async

--- src/SageOneApi/Interfaces/IItemRequest.cs
+++ src/SageOneApi/Interfaces/IItemRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using SageOneApi.Models;
 
 namespace SageOneApi.Interfaces
@@ -22,18 +23,34 @@ namespace SageOneApi.Interfaces
         /// /// <param name="includeAdditionalItemPrices">if set to true Additional Item Prices are included.</param>
         PagingResponse<Item> Get(string filter = "", int skip = 0, bool includeAdditionalItemPrices = false);
 
-		/// <summary>
-		/// Saves the specified item.
-		/// </summary>
-		/// <param name="item">The Item.</param>
-		/// <returns>A <see cref="Item"/> object, populated with updated/new values</returns>
-		Item Save(Item item);
+        /// <summary>
+        /// Gets a collection of items asynchronously.
+        /// </summary>
+        /// <returns>An List collection of <see cref="Item"/> objects</returns>
+        /// <param name="filter">Optional OData Filter Query read more at http://www.odata.org/documentation/odata-version-2-0/uri-conventions/#FilterSystemQueryOption </param>
+        /// <param name="skip">OData skip parameter. Skips the number of records. Used for paging.</param>
+        /// /// <param name="includeAdditionalItemPrices">if set to true Additional Item Prices are included.</param>
+        Task<PagingResponse<Item>> GetAsync(string filter = "", int skip = 0, bool includeAdditionalItemPrices = false);
 
-		/// <summary>
-		/// Deletes an item
-		/// </summary>
-		/// <param name="id">The ID of the item to delete</param>
-		/// <returns>True if successfully deleted, otherwise false</returns>
-		bool Delete(int id);
+        /// <summary>
+        /// Saves the specified item.
+        /// </summary>
+        /// <param name="item">The Item.</param>
+        /// <returns>A <see cref="Item"/> object, populated with updated/new values</returns>
+        Item Save(Item item);
+
+        /// <summary>
+        /// Saves the specified item asynchronously.
+        /// </summary>
+        /// <param name="item">The Item.</param>
+        /// <returns>A <see cref="Item"/> object, populated with updated/new values</returns>
+        Task<Item> SaveAsync(Item item);
+
+        /// <summary>
+        /// Deletes an item
+        /// </summary>
+        /// <param name="id">The ID of the item to delete</param>
+        /// <returns>True if successfully deleted, otherwise false</returns>
+        bool Delete(int id);
 	}
 }
--- src/SageOneApi/Requests/ItemRequest.cs
+++ src/SageOneApi/Requests/ItemRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using RestSharp;
 using RestSharp.Deserializers;
 using RestSharp.Serializers;
@@ -15,7 +16,9 @@ namespace SageOneApi.Requests
 		public Item Get(int id)
 		{
 			var response = _client.Execute<Item>(new RestRequest(string.Format("Item/Get/{0}?apikey={1}&companyid={2}", id, _apiKey, _companyId), Method.GET));
-			return response.Data;
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            return response.Data;
 		}
 
 		public PagingResponse<Item> Get(string filter = "", int skip = 0, bool includeAdditionalItemPrices=false)
@@ -33,28 +36,67 @@ namespace SageOneApi.Requests
 
 			var request = new RestRequest(url, Method.GET);
 			request.RequestFormat = DataFormat.Json;
-
 			var response = _client.Execute(request);
-			JsonDeserializer deserializer = new JsonDeserializer();
-
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            JsonDeserializer deserializer = new JsonDeserializer();
 			return deserializer.Deserialize<PagingResponse<Item>>(response);
 		}
 
-		public Item Save(Item item)
+	    public async Task<PagingResponse<Item>> GetAsync(string filter = "", int skip = 0, bool includeAdditionalItemPrices = false)
+	    {
+            var url = string.Format("Item/Get?apikey={0}&companyid={1}", _apiKey, _companyId);
+
+            if (!string.IsNullOrEmpty(filter))
+                url = string.Format("Item/Get?apikey={0}&companyid={1}&$filter={2}", _apiKey, _companyId, filter);
+
+            if (skip > 0)
+                url += "&$skip=" + skip;
+
+            if (includeAdditionalItemPrices)
+                url += "&includeAdditionalItemPrices=true";
+
+            var request = new RestRequest(url, Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            var response = await _client.ExecuteTaskAsync(request);
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            JsonDeserializer deserializer = new JsonDeserializer();
+
+            return deserializer.Deserialize<PagingResponse<Item>>(response);
+        }
+
+	    public Item Save(Item item)
 		{
 			var url = string.Format("Item/Save?apikey={0}&companyid={1}", _apiKey, _companyId);
 			var request = new RestRequest(url, Method.POST) { JsonSerializer = new JsonSerializer() };
 			request.RequestFormat = DataFormat.Json;
 			request.AddBody(item);
 			var response = _client.Execute<Item>(request);
-			return response.Data;
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            return response.Data;
 		}
 
-		public bool Delete(int id)
+	    public async Task<Item> SaveAsync(Item item)
+	    {
+            var url = string.Format("Item/Save?apikey={0}&companyid={1}", _apiKey, _companyId);
+            var request = new RestRequest(url, Method.POST) { JsonSerializer = new JsonSerializer() };
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(item);
+            var response = await _client.ExecuteTaskAsync<Item>(request);
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            return response.Data;
+        }
+
+	    public bool Delete(int id)
 		{
 			var url = string.Format("Item/Delete/{0}?apikey={1}&companyid={2}", id, _apiKey, _companyId);
 			var response = _client.Execute<Item>(new RestRequest(url, Method.DELETE));
-			return response.ResponseStatus == ResponseStatus.Completed;
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            return response.ResponseStatus == ResponseStatus.Completed;
 		}
 	}
 }
