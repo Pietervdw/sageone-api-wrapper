d7d103c72fb58c5437e0294e2ff832d12421c4b2
Embo1 <embo@outlook.com>
2016-04-04 13:45:06 +0200

    Added support for BankTransaction API method
    Returns transactions for a given company. Currently read-only (Delete /
    Save methods not working)

---
+++ src/SageOneApi/Models/BankTransaction.cs
@@ -0,0 +1,57 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace SageOneApi.Models
+{
+    public class BankTransaction
+    {
+      public int  ID { get; set; }
+      public DateTime Date{get; set;}
+      public int BankAccountId {get; set;} 
+      public int Type{get; set;}
+      public int SelectionId{get; set;} 
+      public string DocumentNumber{get; set;}
+      public string Payee {get; set;} 
+      public string Reference {get; set;} 
+      public string Description {get; set;} 
+      public int TaxTypeId{get; set;} 
+      public decimal Exclusive{get; set;} 
+      public decimal Tax {get; set;} 
+      public decimal Total {get; set;} 
+      public decimal Discount {get; set;} 
+      public bool Reconciled{get; set;} 
+      public string BankUniqueIdentifier {get; set;}
+      public int AnalysisCategoryId1 {get; set;} 
+      public int AnalysisCategoryId2 {get; set;} 
+      public int AnalysisCategoryId3 {get; set;} 
+      public string Memo {get; set;} 
+      public int BankImportMappingId{get; set;} 
+      public int TaxPeriodId{get; set;} 
+      public bool Editable{get; set;} 
+      public List<BankTransaction> SplitTransactions{get; set;} 
+      public bool Accepted {get; set;} 
+      public bool Duplicate {get; set;} 
+      public bool Matched {get; set;} 
+      public string MatchReference{get; set;} 
+      public bool Locked{get; set;} 
+      public bool HasAttachments{get; set;} 
+      public bool HasAllocations {get; set;} 
+      public int PaymentMethodId{get; set;} 
+      public int ImportTypeId{get; set;} 
+      public string TrackingCode{get; set;} 
+      public bool Dirty{get; set;} 
+      public int BankAccountCurrencyId{get; set;} 
+      public decimal BankAccountExchangeRate{get; set;} 
+      public int SelectionCurrencyId{get; set;} 
+      public decimal SelectionExchangeRate {get; set;} 
+      public int TimeAndBillingProjectId{get; set;}
+      public int TimeAndBillingExpenseId{get; set;} 
+      public int BillingMethodId{get; set;} 
+      public decimal Recharge{get; set;} 
+      public decimal MarkupPercentage {get; set;}
+      public DateTime ImportActionDate{get; set;} 
+
+    }
+}
---
+++ src/SageOneApi/Requests/BankTransactionRequest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Deserializers;
+using SageOneApi.Interfaces;
+using SageOneApi.Models;
+
+namespace SageOneApi.Requests
+{
+    public class BankTransactionRequest : RequestBase, IBankTransactionRequest
+    {
+        public BankTransactionRequest(IRestClient client, string apiKey, int companyId)
+            : base(client, apiKey, companyId)
+        {
+        }
+
+        public BankTransaction Get(int id)
+        {
+            var response =
+                _client.Execute<BankTransaction>(
+                    new RestRequest(
+                        string.Format("BankTransaction/Get/{0}?apikey={1}&companyid={2}", id, _apiKey, _companyId),
+                        Method.GET));
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            return response.Data;
+        }
+
+        public PagingResponse<BankTransaction> Get(string filter = "", int skip = 0)
+        {
+            var url = string.Format("BankTransaction/Get?apikey={0}&companyid={1}", _apiKey, _companyId);
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                url = string.Format("BankTransaction/Get?apikey={0}&companyid={1}&$filter={2}", _apiKey, _companyId, filter);                   
+            }
+
+            if (skip > 0)
+            {
+                url += "&$skip=" + skip;
+            }
+
+            var request = new RestRequest(url, Method.GET) {RequestFormat = DataFormat.Json};
+
+            var response = _client.Execute(request);
+            JsonDeserializer deserializer = new JsonDeserializer();
+
+            StatusDescription = response.StatusDescription;
+            StatusCode = response.StatusCode;
+            return deserializer.Deserialize<PagingResponse<BankTransaction>>(response);
+        }
+    }
+}
