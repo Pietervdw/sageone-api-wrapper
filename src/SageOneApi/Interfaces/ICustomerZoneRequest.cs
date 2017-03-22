acb5eaf43a7a1c4edac1b1b332870a89223c8bcd
Pieter van der Westhuizen <pieter@csoft.co.za>
2017-03-22 11:10:04 +0200

    Add missing files

---
+++ src/SageOneApi/Interfaces/ICustomerZoneRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace SageOneApi.Interfaces
+{
+    //
+    public interface ICustomerZoneRequest
+    {
+
+        /// <summary>
+        /// Gets the Customer Zone URL of a specified Invoice
+        /// </summary>
+        /// <param name="invoiceId">The invoice id.</param>
+        /// <param name="customerId">The customer id.</param>
+        /// <returns></returns>
+        string GetCustomerZoneInvoiceUrl(int invoiceId, int customerId);
+
+        /// <summary>
+        /// Gets the Customer Zone URL of a specified Invoice
+        /// </summary>
+        /// <param name="quoteId">The quote id.</param>
+        /// <param name="customerId">The customer id.</param>
+        /// <returns></returns>
+        string GetCustomerZoneQuoteUrl(int quoteId, int customerId);
+
+
+
+    }
+}
---
+++ src/SageOneApi/Interfaces/IQuoteRequest.cs
@@ -0,0 +1,49 @@
+﻿using SageOneApi.Models;
+
+namespace SageOneApi.Interfaces
+{
+    //
+    public interface IQuoteRequest
+    {
+        /// <summary>
+        /// Gets a single quote by ID.
+        /// </summary>
+        /// <param name="id">The ID of the quote.</param>
+        /// <returns>A <see cref="Quote"/> object</returns>
+        /// 
+        Quote Get(int id);
+
+        /// <summary>
+        /// Gets a collection of quotes.
+        /// </summary>
+        /// <param name="includeDetail">if set to <c>true</c> include the lines.</param>
+        /// <param name="includeCustomerDetails">if set to <c>true</c> the Customer is included.</param>
+        /// <param name="filter">Optional OData Filter Query read more at http://www.odata.org/documentation/odata-version-2-0/uri-conventions/#FilterSystemQueryOption </param>
+        /// <param name="skip">OData skip parameter. Skips the number of records. Used for paging.</param>
+        /// <returns></returns>
+        PagingResponse<Quote> Get(bool includeDetail = false, bool includeCustomerDetails = false, string filter = "",
+            int skip = 0);
+
+        /// <summary>
+        /// Saves the specified quote.
+        /// </summary>
+        /// <param name="quote">The quote.</param>
+        /// <returns>A <see cref="Quote"/> object, populated with updated/new values</returns>
+        Quote Save(Quote quote);
+
+        /// <summary>
+        /// Calculates the specified quote total fields
+        /// </summary>
+        /// <param name="quote">The quote.</param>
+        /// <returns>A <see cref="Quote"/> object, populated with updated total fields.</returns>
+        Quote Calculate(Quote quote);
+
+        /// <summary>
+        /// Emails the specified quote.
+        /// </summary>
+        /// <param name="email">The email request.</param>
+        /// <returns>True if successfully sent, otherwise false</returns>
+        bool Email(EmailRequest email);
+
+    }
+}
